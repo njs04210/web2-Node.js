e89fac5f8524c3034a8e09b54bf29da6075ba1c9
njs04210 <68816492+njs04210@users.noreply.github.com>
2020-07-29 23:53:17 +0900

    App제작_글 생성 UI 만들기

--- main.js
+++ main.js
@@ -2,43 +2,50 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body) {
+  return `
+  <!doctype html>
+  <html>
+<head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    <a href="/create">create</a>
+    ${body}
+  </body>
+  </html>
+
+  `;
+}
+
+function templateList(filelist) {
+  var list = '<ul>';
+  var i = 0;
+  while (i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+    i = i + 1;
+  }
+  list = list + '</ul>';
+  return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-
+    console.log(pathname);
     if (pathname === '/') {
-      //home일때의 경우 . web눌렀을 때
+      //home일때의 경우 . web눌렀을 때. 홈으로 들어왔을 때.
       if (queryData.id === undefined ) {
           fs.readdir('./data', function(error, filelist){
 
             var title = "Welcome";
             var description = "Hello, Node.js";
-
-            var list = '<ul>';
-            var i = 0;
-            while (i < filelist.length) {
-              list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-              i = i + 1;
-            }
-            list = list + '</ul>';
-
-            var template = `
-            <!doctype html>
-            <html>
-          <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-
-            `;
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
             response.writeHead(200);
             response.end(template);
           })
@@ -46,42 +53,36 @@ var app = http.createServer(function(request,response){
 //HTML, CSS, JavaScript 눌렀을때. id값이 있는 애
       } else {
         fs.readdir('./data', function(error, filelist){
-        /*
-          var title = "Welcome";
-          var description = "Hello, Node.js";
-*/
-          var list = '<ul>';
-          var i = 0;
-          while (i < filelist.length) {
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-            i = i + 1;
-          }
-          list = list + '</ul>';
-
         fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
           var title = queryData.id;
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-
-          `;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200);
           response.end(template);
         });
       });
     }
-  } else {
+  } else if(pathname === '/create'){ //create 눌렀을 때
+    fs.readdir('./data', function(error, filelist){
+
+      var title = "WEB - create";
+      var list = templateList(filelist);
+      var template = templateHTML(title, list, `
+        <form action="http://localhost:3000/process_create" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `);
+      response.writeHead(200);
+      response.end(template);
+    })
+  }
+  else { //이도저도 아닌것은 404 로 처리
       response.writeHead(404);
       response.end('Not found');
     }
