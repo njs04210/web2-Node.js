549c89da486b21f5ab0d6d90e47ba9810f0164d2
njs04210 <68816492+njs04210@users.noreply.github.com>
2020-08-02 21:04:09 +0900

    App제작_템플릿 기능 정리정돈 하기
    함수들을 객체 안에 넣어 정리함

--- main.js
+++ main.js
@@ -3,41 +3,42 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body, control) {
-  return `
-  <!doctype html>
-  <html>
-<head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    ${control}
-    ${body}
-  </body>
-  </html>
+var template = { //객체에 있는 값 하나하나 = property. html은 propery의 이름.
+  HTML: function(title, list, body, control) {
+    return `
+    <!doctype html>
+    <html>
+  <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      ${list}
+      ${control}
+      ${body}
+    </body>
+    </html>
 
-  `;
-}
-
-function templateList(filelist) {
-  var list = '<ul>';
-  var i = 0;
-  while (i < filelist.length) {
-    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-    i = i + 1;
+    `;
+  },
+  list: function(filelist) {
+    var list = '<ul>';
+    var i = 0;
+    while (i < filelist.length) {
+      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+      i = i + 1;
+    }
+    list = list + '</ul>';
+    return list;
   }
-  list = list + '</ul>';
-  return list;
 }
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    console.log(pathname);
+    //console.log(pathname);
     if (pathname === '/') {
       //home일때의 경우 . web눌렀을 때. 홈으로 들어왔을 때.
       if (queryData.id === undefined ) {
@@ -45,19 +46,19 @@ var app = http.createServer(function(request,response){
 
             var title = "Welcome";
             var description = "Hello, Node.js";
-            var list = templateList(filelist);
-            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
+            var list = template.list(filelist);
+            var html = template.HTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
             response.writeHead(200);
-            response.end(template);
-          })
+            response.end(html);
+          });
 
 //HTML, CSS, JavaScript 눌렀을때. id값이 있는 애
       } else {
         fs.readdir('./data', function(error, filelist){
         fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
           var title = queryData.id;
-          var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`,
+          var list = template.list(filelist);
+          var html = template.HTML(title, list, `<h2>${title}</h2>${description}`,
           ` <a href="/create">create</a>
             <a href="/update?id=${title}">update</a>
             <form action="/delete_process" method="post">
@@ -66,7 +67,7 @@ var app = http.createServer(function(request,response){
             </form>`
           );
           response.writeHead(200);
-          response.end(template);
+          response.end(html);
         });
       });
     }
@@ -74,8 +75,8 @@ var app = http.createServer(function(request,response){
     fs.readdir('./data', function(error, filelist){
 
       var title = "WEB - create";
-      var list = templateList(filelist);
-      var template = templateHTML(title, list, `
+      var list = template.list(filelist);
+      var html = template.HTML(title, list, `
         <form action="/create_process" method="post">
           <p><input type="text" name="title" placeholder="title"></p>
           <p>
@@ -87,7 +88,7 @@ var app = http.createServer(function(request,response){
         </form>
         `, '');
       response.writeHead(200);
-      response.end(template);
+      response.end(html);
     })
   } else if(pathname === '/create_process') { //제출 버튼 눌렀을 때
 
@@ -112,8 +113,8 @@ var app = http.createServer(function(request,response){
       fs.readdir('./data', function(error, filelist){
       fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
         var title = queryData.id;
-        var list = templateList(filelist);
-        var template = templateHTML(title, list,
+        var list = template.list(filelist);
+        var html = template.HTML(title, list,
           `
           <form action="/update_process" method="post">
             <input type="hidden" name="id" value="${title}">
@@ -128,7 +129,7 @@ var app = http.createServer(function(request,response){
           `,
           `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
         response.writeHead(200);
-        response.end(template);
+        response.end(html);
       });
     });
   } else if(pathname === '/update_process') {
@@ -159,7 +160,7 @@ var app = http.createServer(function(request,response){
         var post = qs.parse(body);
         var id = post.id;
         fs.unlink(`data/${id}`, function(error) {
-          response.writeHead(302, {Location: `/`}); //삭제하면 홈으로 보내버리게끔 
+          response.writeHead(302, {Location: `/`}); //삭제하면 홈으로 보내버리게끔
           response.end();
         })
     });
